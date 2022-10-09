2530d9513ae6e39386e28ec98a8805ea4053b184
Vitalii Boiko <107396176+vitaliiseeker@users.noreply.github.com>
2022-10-10 01:48:30 +0300

    update code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,4 +3,16 @@ import React, { Component } from "react";
 
 export class App extends Component {
   state = {
-     }
+    a: 0
+  }
+
+
+  render() {
+
+    return (
+      <>
+        <p>ABC</p>
+      </>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,22 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-// import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
-// import theme from './theme';
-// import { GlobalStyle } from 'theme/GlobalStyle';
-
-//   <React.StrictMode>
-//     <ThemeProvider theme={theme}>
-//       <GlobalStyle />
-//       <App />
-//     </ThemeProvider>
-//   </React.StrictMode>
-// );
